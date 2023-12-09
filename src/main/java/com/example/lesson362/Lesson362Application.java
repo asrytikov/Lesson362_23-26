f0179ee809a6afbb88b93ecd62e741ad67c0d0cd
asrytikov <asrytikov@gmail.com>
2023-12-09 20:12:08 +0400

    Initial commit

---
+++ src/main/java/com/example/lesson362/Lesson362Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson362;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson362Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson362Application.class, args);
+    }
+
+}
